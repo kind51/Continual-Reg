a5e548c866a247d8c1fac88a4b4fde5769d36dc2
kind51 <151921797+kind51@users.noreply.github.com>
2025-05-29 20:35:48 +0800

    OASISCT

--- core/data/data_providers/CTCTDataProvider.py
+++ core/data/data_providers/CTCTDataProvider.py
@@ -1,162 +1,136 @@
 # -*- coding: utf-8 -*-
 """
-
-__author__ == Xinzhe Luo
-__version__ == 0.1
+OASIS 数据集数据提供器（含重采样）
 """
-
 import glob
 import itertools
-import logging
-import os
-# import random
 import numpy as np
 import torch
-import SimpleITK as sitk  # 引入 SimpleITK 库
+import SimpleITK as sitk
 from torch.utils.data import Dataset
-from core.data.image_utils import strsort, load_image_nii
+from core.data.image_utils import strsort, load_image_nii  # 假设存在该工具函数
 from core.data.intensity_augment import randomIntensityFilter
 
 
 class DataProvider(Dataset):
-    """
-    Construct data provider for Task1 Abdomen CT-CT dataset.
-    Validation dataset pattern:
-    |--valid
-    |  |--images
-    |  |  |--AbdomenCTCT_0022_0000.nii.gz
-    |  |  |--AbdomenCTCT_0023_0000.nii.gz
-    |  |  |--...
-    |  |--labels
-    |  |  |--AbdomenCTCT_0022_0000.nii.gz
-    |  |  |--AbdomenCTCT_0023_0000.nii.gz
-    |  |  |--...
-
-    """
     dimension = 3
 
-    def __init__(self, data_search_path, training=False, **kwargs):
+    def __init__(self, data_search_path, training=True, **kwargs):
         self.data_search_path = data_search_path
         self.training = training
         self.kwargs = kwargs
-        self.ct_suffix = self.kwargs.pop('ct_suffix', '0000.nii.gz')
-        self.ct_range = self.kwargs.pop('ct_range', [-200, 300])
-        self.image_prefix = self.kwargs.pop('image_prefix', 'images')
-        self.label_prefix = self.kwargs.pop('label_prefix', 'labels')
-        self.intensity_aug = self.kwargs.pop('intensity_aug', False)
-        self.equalize_hist = self.kwargs.pop('equalize_hist', False)
-        self.crop_shape = np.asarray(self.kwargs.pop('crop_shape', (112, 96, 112)),
-                                     dtype=np.int32)
+        self.mr_suffix = kwargs.pop('mr_suffix', '0000.nii.gz')
+        self.mr_range = kwargs.pop('mr_range', [-np.inf, np.inf])
+        self.image_prefix = kwargs.pop('image_prefix', 'images')
+        self.label_prefix = kwargs.pop('label_prefix', 'labels')
+        self.mask_prefix = kwargs.pop('mask_prefix', 'masks')
+        self.intensity_aug = kwargs.pop('intensity_aug', False)
+        self.pad_shape = np.asarray(kwargs.pop('crop_shape', (112, 96, 112)), dtype=np.int32)
+        self.max_length = kwargs.pop('max_length', 10000)  # 控制数据集大小
 
-        self.data_pair_names = self._find_data_names(self.data_search_path)
+        # 查找所有图像对
+        self.data_pair_names = self._find_data_names(data_search_path)
 
     def __len__(self):
-        return len(self.data_pair_names)
-
-    def get_image_name(self, index):
-        return self.data_pair_names[index]
+        return min(len(self.data_pair_names), self.max_length)
 
     def _find_data_names(self, data_search_path):
-        """
-        Get pairs of image names.
+        """获取所有图像对路径"""
+        all_nii_names = strsort(glob.glob(os.path.join(data_search_path, '**/*.nii.gz'), recursive=True))
+        all_img_names = [name for name in all_nii_names if self.image_prefix in name]
+        mr_img_names = [name for name in all_img_names if self.mr_suffix in os.path.basename(name)]
 
-        :param data_search_path:
-        :return:
-        """
-        all_nii_names = strsort(glob.glob(os.path.join(data_search_path,
-                                                       '**/*.nii.gz'),
-                                          recursive=True))
-        all_nii_names = [os.path.normpath(name) for name in all_nii_names]
-        all_img_names = [name for name in all_nii_names if name.split(os.path.sep)[-2] == self.image_prefix]
-
-        CT_img_names = [
-            name for name in all_img_names if self.ct_suffix in os.path.basename(name)
-        ]
-
+        # 生成训练/验证对（训练集为笛卡尔积，验证集为排列）
         if self.training:
-            pair_names = list(itertools.product(CT_img_names, CT_img_names))
+            pair_names = list(itertools.product(mr_img_names, mr_img_names))
         else:
-            pair_names = list(itertools.permutations(CT_img_names, r=2))
-
+            pair_names = list(itertools.permutations(mr_img_names, 2))
         return pair_names
 
-    def resample_image(self, image, new_spacing):
-        """
-        重采样图像到指定的间距
-        :param image: SimpleITK 图像对象
-        :param new_spacing: 新的图像间距
-        :return: 重采样后的 SimpleITK 图像对象
-        """
-        original_spacing = image.GetSpacing()
-        original_size = image.GetSize()
+    def resample_image(self, image_path, new_spacing):
+        """重采样图像到指定间距"""
+        img_itk = sitk.ReadImage(image_path)
+        original_spacing = img_itk.GetSpacing()
+        original_size = img_itk.GetSize()
+
+        # 计算新尺寸
         new_size = [int(round(osz * ospc / nspc)) for osz, ospc, nspc in
                     zip(original_size, original_spacing, new_spacing)]
+
+        # 重采样设置
         resampler = sitk.ResampleImageFilter()
         resampler.SetOutputSpacing(new_spacing)
         resampler.SetSize(new_size)
-        resampler.SetOutputDirection(image.GetDirection())
-        resampler.SetOutputOrigin(image.GetOrigin())
+        resampler.SetOutputDirection(img_itk.GetDirection())
+        resampler.SetOutputOrigin(img_itk.GetOrigin())
         resampler.SetTransform(sitk.Transform())
-        resampler.SetDefaultPixelValue(image.GetPixelIDValue())
-        resampler.SetInterpolator(sitk.sitkBSpline)
-        return resampler.Execute(image)
+        resampler.SetDefaultPixelValue(img_itk.GetPixelIDValue())
+        resampler.SetInterpolator(sitk.sitkBSpline)  # 插值方式
+        return resampler.Execute(img_itk), img_itk.GetDirection()  # 返回重采样后的图像和仿射矩阵
 
     def __getitem__(self, item):
-        pair_names = self.data_pair_names[item]
-        name1, name2 = pair_names
+        name1, name2 = self.data_pair_names[item]
 
-        img1_itk = sitk.ReadImage(name1)
-        img2_itk = sitk.ReadImage(name2)
+        # OASIS 重采样到 2x2x2 mm
+        new_spacing = [2, 2, 2] if self.training else [2, 2, 2]  # 训练/验证均用 2x2x2
+        img1_itk, aff1 = self.resample_image(name1, new_spacing)
+        img2_itk, aff2 = self.resample_image(name2, new_spacing)
 
-        new_spacing = [3, 3, 3]  # 固定为 [3, 3, 3] 的间距
-        img1_itk = self.resample_image(img1_itk, new_spacing)
-        img2_itk = self.resample_image(img2_itk, new_spacing)
-
+        # 转换为 numpy 数组
         img1 = sitk.GetArrayFromImage(img1_itk)
         img2 = sitk.GetArrayFromImage(img2_itk)
 
-        img1 = np.clip(img1, a_min=self.ct_range[0], a_max=self.ct_range[1])
-        img2 = np.clip(img2, a_min=self.ct_range[0], a_max=self.ct_range[1])
+        # 强度裁剪（MR 通常不需要固定范围，使用百分位裁剪）
+        img1 = np.clip(img1, a_min=np.percentile(img1, 1), a_max=np.percentile(img1, 99))
+        img2 = np.clip(img2, a_min=np.percentile(img2, 1), a_max=np.percentile(img2, 99))
 
-        if np.min(img1) < 0:
-            img1 = img1 - np.min(img1)
-        if np.min(img2) < 0:
-            img2 = img2 - np.min(img2)
-
+        # 强度增强（可选）
         if self.intensity_aug:
             img1 = randomIntensityFilter(img1)
             img2 = randomIntensityFilter(img2)
 
+        # 加载标签和掩码
         lab1 = load_image_nii(name1.replace(self.image_prefix, self.label_prefix))[0]
         lab2 = load_image_nii(name2.replace(self.image_prefix, self.label_prefix))[0]
+        mask1 = load_image_nii(name1.replace(self.image_prefix, self.mask_prefix))[0]
+        mask2 = load_image_nii(name2.replace(self.image_prefix, self.mask_prefix))[0]
 
-        images = np.stack([img1, img2])  # [2, *vol_shape]
-        labels = np.stack([lab1, lab2])  # [2, *vol_shape]
+        # 调整维度顺序：[C, D, H, W]
+        images = np.stack([img1, img2]).transpose((0, 3, 1, 2))  # [2, D, H, W]
+        labels = np.stack([lab1, lab2]).transpose((0, 3, 1, 2))
+        masks = np.stack([mask1, mask2]).transpose((0, 3, 1, 2))
 
-        # crop roi
-        half = np.asarray(images.shape[-self.dimension:]) // 2
-        r = self.crop_shape // 2
-        images = images[..., half[0] - r[0]:half[0] + r[0],
-                 half[1] - r[1]:half[1] + r[1], half[2] - r[2]:half[2] + r[2]]
-        labels = labels[..., half[0] - r[0]:half[0] + r[0],
-                 half[1] - r[1]:half[1] + r[1], half[2] - r[2]:half[2] + r[2]]
+        # 填充到固定尺寸 (112, 96, 112)
+        current_shape = np.array(images.shape[1:])  # [D, H, W]
+        pad_width = [(0, 0)] + [((self.pad_shape[i] - current_shape[i]) // 2,
+                                 self.pad_shape[i] - current_shape[i] - (self.pad_shape[i] - current_shape[i]) // 2)
+                                for i in range(3)]
+        images = np.pad(images, pad_width, mode='constant')
+        labels = np.pad(labels, pad_width, mode='constant')
+        masks = np.pad(masks, pad_width, mode='constant')
 
-        names = [os.path.basename(name)[:-7] for name in pair_names]
-        names = '_'.join(names)
+        # 获取头信息（假设 load_image_nii 可返回头信息，或从 ITK 图像获取）
+        head1 = img1_itk.GetMetaDataDictionary()  # 若 SimpleITK 图像有元数据
+        head2 = img2_itk.GetMetaDataDictionary()
 
         return {
-            'images': images,
-            'labels': labels,
-            'affines': [img1_itk.GetDirection(), img2_itk.GetDirection()],
-            'headers': [img1_itk.GetMetaDataDictionary(), img2_itk.GetMetaDataDictionary()],
-            'names': names,
+            'images': images.astype(np.float32),
+            'labels': labels.astype(np.float32),
+            'masks': masks.astype(np.float32),
+            'affines': [aff1, aff2],
+            'headers': [head1, head2],
+            'names': f"{os.path.basename(name1)[:-7]}_{os.path.basename(name2)[:-7]}"
         }
 
     def data_collate_fn(self, batch):
-        AF = [data.pop('affines') for data in batch]
-        HE = [data.pop('headers') for data in batch]
-        batch_tensor = dict([(k, torch.stack([torch.from_numpy(data[k]) for data in batch])) for k in batch[0].keys()])
-        batch_tensor['affines'] = AF
-        batch_tensor['headers'] = HE
-
+        """数据整理函数"""
+        affines = [data.pop('affines') for data in batch]
+        headers = [data.pop('headers') for data in batch]
+        batch_tensor = {
+            k: torch.from_numpy(np.stack([data[k] for data in batch])).float()
+            for k in batch[0].keys() if k not in ['affines', 'headers', 'names']
+        }
+        batch_tensor['affines'] = affines
+        batch_tensor['headers'] = headers
+        batch_tensor['names'] = [data['names'] for data in batch]
         return batch_tensor
